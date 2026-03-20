8245a9468b66c450d90ec968798f7b957f3124a5
ishandrai07 <mywork4ever.77@gmail.com>
2026-03-20 10:17:41 +0530

    Add files via upload

---
+++ 31-js conditionals/conditionals.js
@@ -0,0 +1,23 @@
+console.log("conditionals");
+
+
+
+let age = 19;
+
+if(age > 18){
+    console.log("you can drive");
+}
+
+else{
+    console.log("you cannot drive");
+}
+
+{
+    let a = 3;
+    console.log(a ** 2);       // exponential 
+}
+
+var b = "3";
+var c = 3;
+console.log(b == c);    // Compares values only , Automatically converts data types
+console.log(b === c);   // Compares value + data type
