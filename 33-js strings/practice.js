6c36fdf9c2a5e910bb24fec7ae12b27e7160b25b
ishandrai07 <mywork4ever.77@gmail.com>
2026-07-13 00:53:10 +0530

    Add files via upload

---
+++ 33-js strings/practice.js
@@ -0,0 +1,38 @@
+// Business Name Generator without using array
+
+let rand = Math.random();
+let first , second , third;
+
+if(rand < 0.33){
+    first = "Crazy";
+}
+else if(rand < 0.66 && rand >= 0.33){
+    first = "Amazing";
+}
+else{
+    first = "Fire";
+}
+
+rand = Math.random();
+if(rand < 0.33){
+    second = "Engine";
+}
+else if(rand < 0.66 && rand >= 0.33){
+    second = "Food";
+}
+else{
+    second = "Garments";
+}
+
+rand = Math.random();
+if(rand < 0.33){
+    third = "Bros";
+}
+else if(rand < 0.66 && rand >= 0.33){
+    third = "Limited";
+}
+else{
+    third = "Hub";
+}
+
+console.log(`${first} ${second} ${third}`);
---
+++ 33-js strings/string.js
@@ -0,0 +1,26 @@
+console.log("Strings");
+
+let a = "ishand";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length);   //length of string or array
+
+let your_name = "ishand";
+let frn_name = "ishaan";
+console.log("your name is " + your_name + " and your friend name is " + frn_name);
+console.log(`your name is ${your_name} and your friend name is ${frn_name}`);
+
+
+let b = "javaScript";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+
+console.log(b.slice(1,4));
+console.log(b.replace("java" , "type"));
+
+console.log(b.concat(a,"rahul", "aditya"));
