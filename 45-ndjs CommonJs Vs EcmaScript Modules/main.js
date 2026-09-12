6ac06bebded578eff37ef66064cb40e3678d841a
ishandrai07 <mywork4ever.77@gmail.com>
2026-09-12 23:54:47 +0530

    Add files via upload

---
+++ 45-ndjs CommonJs Vs EcmaScript Modules/main.js
@@ -0,0 +1,11 @@
+export const a = 2
+export const b = 4
+export const c = 6
+export const d = 8
+
+
+const obj = {
+    x:5,
+    y:7
+}
+export  default obj;
---
+++ 45-ndjs CommonJs Vs EcmaScript Modules/server.js
@@ -0,0 +1,25 @@
+// import http from "http"
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+
+// import {a , b} from "./main.js"
+
+// console.log(a ,b);
+
+
+
+import anyname from "./main.js";
+console.log(anyname);
